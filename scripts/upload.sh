ddd0deaac392778e652efb4ac0f1a26f4098697a
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-13 00:59:16 -0700

    Added: scripts/env_vars.sh scripts/presign.py Deleted: .version_tag scripts/update_installer_data.sh scripts/upload_to_r2.py Modified: .gitignore scripts/upload.sh

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -11,9 +11,13 @@ PKG="nixos-asahi-${DATE_TAG}.zip"
 ROOTSIZE=$(cat "${RESULT}"/.root_part_size)
 TMP=$(mktemp -d /tmp/nixos-asahi-package.XXXXXXXXXX)
 
+export RESULT BASEURL DATE_TAG PKG ROOTSIZE TMP
+
+test -e ./scripts/secrets.sh && source ./scripts/secrets.sh
+
 confirm() {
   while true; do
-    read -r -n 1 -p "${1:-Continue?} [y/n]: " REPLY
+    read -r -n 1 -p "$1 [y/n]: " REPLY
     case $REPLY in
       [yY]) echo ; return 0 ;;
       [nN]) echo ; return 1 ;;
@@ -23,51 +27,51 @@ confirm() {
 }
 
 upload() {
-  if [[ -e ${RESULT}/${PKG} ]]; then
-    cp -a "${RESULT}"/"${PKG}" "${TMP}"
-    chmod 644 "${TMP}/${PKG}"
-  else
-    echo 'error: ${PKG}: file not found'; exit 1
-  fi
-  if (python3 scripts/upload_to_r2.py || exit 1); then
+  if (curl --progress-bar \
+    --request PUT \
+    --url "${PRESIGNED_URL}" \
+    --header "Content-Type: application/zip" \
+    --header "accept: application/json" \
+    --data-binary @"${TMP}/${PKG}" | cat)
+  then
     echo "Success! ${PKG} uploaded to bucket."
-    rm -rf "${TMP}"
-  fi
-}
-
-update_installer_data() {
-  jq -r < ./data/template/installer_data.json ".[].[].package = \"${BASEURL}/${PKG}\" | .[].[].partitions.[1].size = \"${ROOTSIZE}B\" | .[].[].name = \"NixOS Asahi Package ${DATE_TAG}\"" > ./data/installer_data.json
-}
-
-main() {
-  if [[ -d "${TMP}"/venv ]]; then
-    source "${TMP}"/venv/bin/activate
-  else
-    python3 -m venv "${TMP}"/venv && source "${TMP}"/venv/bin/activate
-    python3 -m pip install boto3 tqdm colorthon
-  fi
-
-  if [[ ! -e scripts/env_vars.sh ]]; then
-    echo 'error: env vars file not found'; exit 1
-  else
-    source scripts/env_vars.sh
-  fi
-
-  confirm "Begin upload?" || exit 0
-
-  if (upload || exit 1); then
-    confirm "Update package version and push to git?" || exit 0
   else
     exit 1
   fi
-
-  # update_installer_data || exit 1
-
-  # if git add ./data/installer_data.json; then
-  #   git commit -m "release: NixOS Asahi-Installer Package ${DATE_TAG}"
-  #   git tag "release-${DATE_TAG}"
-  #   git push -u origin "release-${DATE_TAG}"
-  # fi
 }
 
-main || exit 1
+if [[ -e ${RESULT}/${PKG} ]]; then
+  cp -a "${RESULT}"/"${PKG}" "${TMP}"
+  chmod 644 "${TMP}/${PKG}"
+else
+  echo "error: ${PKG}: file not found"; exit 1
+fi
+
+if [[ -d "${TMP}"/venv ]]; then
+  source "${TMP}"/venv/bin/activate
+else
+  python3 -m venv "${TMP}"/venv && source "${TMP}"/venv/bin/activate
+  python3 -m pip install boto3
+fi
+
+read -r PRESIGNED_URL < <(python3 scripts/presign.py 2>/dev/null)
+
+echo
+confirm "Begin upload?" || exit 0
+echo
+
+if upload; then
+  confirm "Update installer data and push to git?" || exit 0
+
+  jq -r < ./data/template/installer_data.json \
+    ".[].[].package = \"${BASEURL}/${PKG}\" | .[].[].partitions.[1].size = \"${ROOTSIZE}B\" | .[].[].name = \"NixOS Asahi Package ${DATE_TAG}\"" \
+    > ./data/installer_data.json
+
+    git add ./data/installer_data.json
+    git commit -m "release: NixOS Asahi-Installer Package ${DATE_TAG}"
+    git tag "release-${DATE_TAG}"
+    git push -u origin "release-${DATE_TAG}"
+fi
+
+unset RESULT DATE_TAG PKG TMP
+rm -rf "${TMP}"
