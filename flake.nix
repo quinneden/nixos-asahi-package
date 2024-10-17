8f5ddea3e3642bc50188858af5f7de6569e9f58d
Quinn Edenfield <quinnyxboy@gmail.com>
2024-10-16 19:11:28 -0700

    Added: .gitattributes scripts/secrets.sh Modified: .gitignore flake.nix

--- flake.nix
+++ flake.nix
@@ -18,51 +18,71 @@
     };
   };
 
-  outputs = {
-    nixos-apple-silicon,
-    nixos-generators,
-    lix-module,
-    nixpkgs,
-    self,
-    ...
-  } @ inputs: let
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "aarch64-darwin"
-      ] (system:
-        function (import nixpkgs {
-          system = "aarch64-linux";
-          config.allowUnfree = true;
-          overlays = [nixos-apple-silicon.overlays.default];
-        }));
-  in {
-    packages = forAllSystems ({
-      system,
-      pkgs,
+  outputs =
+    {
+      nixos-apple-silicon,
+      nixos-generators,
+      lix-module,
+      nixpkgs,
+      self,
       ...
-    }: {
-      default = self.packages.${system}.asahiPackage;
-      asahiPackage = pkgs.callPackage ./generate-package.nix {inherit self pkgs;};
-      asahiImage = nixos-generators.nixosGenerate {
-        inherit system pkgs;
-        # system = "aarch64-linux";
-        # pkgs = import nixpkgs {
-        #   system = "aarch64-linux";
-        #   config.allowUnfree = true;
-        #   overlays = [nixos-apple-silicon.overlays.default];
-        # };
-        specialArgs = {inherit inputs;};
-        modules = [
-          ({...}: {nix.registry.nixpkgs.flake = nixpkgs;})
-          nixos-apple-silicon.nixosModules.default
-          lix-module.nixosModules.lixFromNixpkgs
-          ./configuration.nix
-        ];
-        format = "raw-efi";
-      };
-    });
+    }@inputs:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-linux"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            function (
+              import nixpkgs {
+                system = "aarch64-linux";
+                config.allowUnfree = true;
+                overlays = [ nixos-apple-silicon.overlays.default ];
+              }
+            )
+          );
+    in
+    {
+      packages = forAllSystems (
+        {
+          system,
+          pkgs,
+          ...
+        }:
+        {
+          default = self.packages.${system}.asahiPackage;
+          asahiPackage = pkgs.callPackage ./generate-package.nix { inherit self pkgs; };
+          asahiImage = nixos-generators.nixosGenerate {
+            inherit system pkgs;
+            # system = "aarch64-linux";
+            # pkgs = import nixpkgs {
+            #   system = "aarch64-linux";
+            #   config.allowUnfree = true;
+            #   overlays = [nixos-apple-silicon.overlays.default];
+            # };
+            specialArgs = {
+              inherit inputs;
+            };
+            modules = [
+              (
+                { ... }:
+                {
+                  nix.registry.nixpkgs.flake = nixpkgs;
+                }
+              )
+              nixos-apple-silicon.nixosModules.default
+              lix-module.nixosModules.lixFromNixpkgs
+              ./configuration.nix
+            ];
+            format = "raw-efi";
+          };
+        }
+      );
 
-    templates.default = inputs.nixos-asahi-starter.templates.default;
-  };
+      templates.default = inputs.nixos-asahi-starter.templates.default;
+    };
 }
