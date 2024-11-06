dc12bb111b4b94ff272c53b42737a345d9b7dd1a
quinneden <quinnyxboy@gmail.com>
2024-11-06 02:49:59 -0800

    Modified: flake.lock, nixos/default.nix

--- nixos/default.nix
+++ nixos/default.nix
@@ -59,7 +59,7 @@
 
   fileSystems."/" = {
     device = lib.mkForce "/dev/disk/by-uuid/f222513b-ded1-49fa-b591-20ce86a2fe7f";
-    fsType = "btrfs";
+    fsType = lib.mkForce "btrfs";
   };
 
   fileSystems."/boot" = {
