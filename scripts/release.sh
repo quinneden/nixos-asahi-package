4e550e2d4ecf57e959899ca8623b35f863572481
quinneden <quinnyxboy@gmail.com>
2025-01-14 05:42:05 -0800

    Modified: scripts/release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,7 +2,7 @@
 
 version_tag="v$(nix eval --raw .#installerPackage.version)"
 
-if ! git tag --list | grep "$version_tag"; then
+if ! (git tag --list | grep "$version_tag"); then
   git tag "$version_tag"
-  git push --tags "$version_tag"
+  git push --tags origin "$version_tag"
 fi
