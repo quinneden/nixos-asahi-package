1ecb9ed0404cd064645a57afebbe056005aaf61e
quinneden <quinnyxboy@gmail.com>
2025-01-20 01:24:11 -0800

    Modified: scripts/release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
-version_tag="v$(nix eval --raw .#installerPackage.version)"
+version="v$(nix eval --raw .#installerPackage.version)"
 
-if ! (git tag --list | grep "$version_tag"); then
-  git tag "$version_tag"
-  git push --tags origin "$version_tag"
+if [[ ! -f "flake.nix" ]]; then
+  echo "This script must be run from the root of the repository" >&2
+  exit 1
 fi
+
+if [[ "$(git symbolic-ref --short HEAD)" != "main" ]]; then
+  echo "must be on main branch" >&2
+  exit 1
+fi
+
+uncommited_changes=$(git diff --compact-summary)
+if [[ -n "$uncommited_changes" ]]; then
+  echo -e "There are uncommited changes, exiting:\n${uncommited_changes}" >&2
+  exit 1
+fi
+
+if (git tag --list | grep "$version"); then
+  echo "version already exists" >&2
+  exit 1
+fi
+
+git commit -am "release: v$version"
+git tag -a "v$version" -m "release: v$version"
+git tag -d "latest"
+git tag -a "latest" -m "release: v$version"
