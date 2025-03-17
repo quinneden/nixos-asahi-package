a7212ae8401a1446aa98b7528a72f358b694555c
quinneden <quinnyxboy@gmail.com>
2025-03-16 17:02:45 -0700

    upload.nix, upload.py: move python script to external file

--- scripts/upload.nix
+++ scripts/upload.nix
@@ -4,50 +4,6 @@
   ...
 }:
 
-let
-  uploadPy = pkgs.writeScript "upload.py" ''
-    import boto3
-    import os
-    from botocore.config import Config
-
-    pkg_zip = "${installerPackage.name}.zip"
-    pkg_data = "installer_data.json"
-
-    obj_list = [pkg_zip, pkg_data]
-
-    s3 = boto3.client(
-        "s3",
-        region_name="auto",
-        config=Config(signature_version="s3v4"),
-        aws_access_key_id=os.getenv("ACCESS_KEY_ID"),
-        aws_secret_access_key=os.getenv("SECRET_ACCESS_KEY"),
-        endpoint_url=os.getenv("ENDPOINT_URL"),
-    )
-
-    transfer_config = boto3.s3.transfer.TransferConfig(
-        multipart_threshold=8 * 1024 * 1024,
-        multipart_chunksize=8 * 1024 * 1024,
-    )
-
-    def upload_to_r2(file):
-        content_type = (
-            "text/plain" if file.endswith(".json") else "application/octet-stream"
-        )
-        prefix = "data" if file.endswith(".json") else "os"
-        with open(file, "rb") as fb:
-            s3.upload_fileobj(
-                fb,
-                ExtraArgs={"ContentType": content_type},
-                Bucket=os.getenv("BUCKET_NAME"),
-                Key=os.path.join(prefix, file),
-                Config=transfer_config,
-            )
-
-    for obj in obj_list:
-        upload_to_r2(obj)
-  '';
-in
-
 pkgs.writeShellApplication {
   name = "upload-to-cdn";
 
@@ -59,7 +15,7 @@ pkgs.writeShellApplication {
 
   text = ''
     pkgData="installer_data-${installerPackage.version}.json"
-    pkgZip="${installerPackage.name}.zip"
+    pkgZip="${installerPackage.name}.zip"; export pkgZip
     tmpDir=$(mktemp -d)
 
     trap 'rm -rf $tmpDir' EXIT
@@ -93,7 +49,7 @@ pkgs.writeShellApplication {
     jq '.os_list += [input]' os_list.json "$pkgData" > installer_data.json
 
     echo "Uploading package and installer data to bucket..."
-    python3 ${uploadPy} || exit 1
+    python3 ${./upload.py} || exit 1
     echo "Done!"
     popd > /dev/null
   '';
